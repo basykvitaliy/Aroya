42598e4dfbe9a3dc3534285fc41401d0404ee378
Dmytro Korovin <d.korovin@rocketech.it>
2025-01-24 15:46:41 +0200

    added Colors

---
+++ lib/ui/style/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static const Color mainDarkBlue = Color(0xFF363E67);
+  static const Color mainBlue = Color(0xFF4C5480);
+  static const Color oceanBlue = Color(0xFF217DD0);
+  static const Color oceanBlue500 = Color(0xFF36B0FD);
+  static const Color oceanBlue300 = Color(0xFF88D5FC);
+  static const Color oceanBlue200 = Color(0xFFC5E8FF);
+  static const Color oceanBlue150 = Color(0xFFD7EDFF);
+  static const Color oceanGreen1 = Color(0xFF00C485);
+  static const Color oceanGreen2 = Color(0xFF3ABA5C);
+  static const Color lightDarkBlue = Color(0xFFE4E6F5);
+  static const Color lightBlue = Color(0xFFF2F7FF);
+  static const Color lightGreen = Color(0xFFD5F3EA);
+  static const Color lightGreenClick = Color(0xFFC0FDEB);
+  static const Color lightSteel = Color(0xFFECEFF3);
+  static const Color lightRed = Color(0xFFFBE4E4);
+  static const Color greyScale800 = Color(0xFF20293A);
+  static const Color greyScale600 = Color(0xFF4A5567);
+  static const Color greyScale500 = Color(0xFF677389);
+  static const Color greyScale400 = Color(0xFF97A2B6);
+  static const Color greyScale300 = Color(0xFFCDD5E0);
+  static const Color greyScale200 = Color(0xFFE3E8EF);
+  static const Color greyScale100 = Color(0xFFF2F5F9);
+  static const Color greyScale50 = Color(0xFFF8FAFC);
+}
